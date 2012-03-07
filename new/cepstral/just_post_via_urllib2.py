feadec909fe093306bd651a6570966d5254bb936
Asheesh Laroia <asheesh@asheesh.org>
2012-03-07 11:34:30 -0800

    Add new urllib2 demo file

---
+++ new/cepstral/just_post_via_urllib2.py
@@ -0,0 +1,24 @@
+import urllib2
+import urllib
+import BeautifulSoup
+
+# The URL to this service
+URL = 'http://www.cepstral.com/cgi-bin/demos/weather'
+
+
+def main():
+    # Here is the data that FireBug said we sent
+    postdict = {'city' : 'San Francisco',
+                'demotype' : 'actual',
+                'state' : 'CA',
+                'voice' : 'David',
+                'submit':'Synthesize the weather'}
+
+    # Encode it into HTTP form
+    # paying scrupulous attention to e.g. Unicode
+    postme = urllib.urlencode(postdict)
+
+    # Send it...
+    fd = urllib2.urlopen(URL, postme)
+    return fd
+
