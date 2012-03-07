24c8cacdc9d103e0645c66a0ef13870232ed1d63
Asheesh Laroia <asheesh@asheesh.org>
2012-03-07 11:23:43 -0800

    Actually play it, just for fun

--- new/cepstral/all_together.py
+++ new/cepstral/all_together.py
@@ -1,15 +1,20 @@
 import just_post_via_mechanize
 import urlparse
 import stupid_string_parse
+import os
 
-def get_wav_link():
+def get_relative_wav_link():
     fp = just_post_via_mechanize.main()
     as_string = fp.read()
     wav_link = stupid_string_parse.parse(as_string)
     return wav_link
 
-def main():
-    return urlparse.urljoin('http://cepstral.com/cgi-bin/demos/weather', get_wav_link())
+def get_absolute_wav_link():
+    return urlparse.urljoin('http://cepstral.com/cgi-bin/demos/weather', get_relative_wav_link())
+
+def play_it():
+    url = get_absolute_wav_link()
+    os.system("mplayer " + url) # UNSAFE
 
 if __name__ == '__main__':
-    print main()
+    play_it()
