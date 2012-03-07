ce7d5efc8edb562ea212366296dbb89640583a1f
Asheesh Laroia <asheesh@asheesh.org>
2012-03-07 11:23:35 -0800

    Stop returning twice for no reason

--- new/cepstral/all_together_lxml.py
+++ new/cepstral/all_together_lxml.py
@@ -6,8 +6,8 @@ def get_wav_link():
     fp = just_post_via_mechanize.main()
     doc = lxml.html.parse(fp).getroot()
     link_targets = [link.attrib.get('href', '') for link in doc.cssselect('a')]
-    return [target for target in link_targets if 'wav' in target][0]
-    return thing
+    wav_links = [target for target in link_targets if 'wav' in target]
+    return wav_links[0]
 
 def main():
     return urlparse.urljoin('http://cepstral.com/cgi-bin/demos/weather', get_wav_link())
